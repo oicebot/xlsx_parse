d1e4d79773ac7310dba6c142b033703b548b7610
Oicebot <nervcn09@gmail.com>
2017-05-02 22:25:47 +0800

    倘若描述数据比病理诊断还少，将会出现严重故障，这是一个尝试性修复
    如果位置填满了，那么用函数跑个逻辑判断，起码病理在右边，我们可以把左边的描述隐藏掉了，没有病理结果这个不用看的

--- xlsx_parse.py
+++ xlsx_parse.py
@@ -143,15 +143,23 @@ if __name__ == '__main__':
     ws = wb.active
     
     line_index = 0
+    lastrow=[]
     for row in data1:
         outrow = list(row)
         if line_index < len(data2):
             #第一行标题续后，之后只有编号相同的续上
             if line_index == 0 or data2[line_index][0] == outrow[0]:
-                outrow.extend(data2[line_index][4:])  #跳过 '编号','姓名','年龄','住院号',
+                outrow.extend(data2[line_index][4:]) #跳过 '编号','姓名','年龄','住院号',
                 line_index += 1
+            #//TODO: 倘若描述数据比病理诊断还少，将会出现严重故障，下面是一个尝试性修复：
+            elif data2[line_index][0] <= outrow[0]:
+                if lastrow:                               #试着把上一行的前面几列复制过来，前提是上一行不为空，否则可能会出错
+                    outrow = lastrow[:15]                 #复制前15列内容
+                    outrow.extend(data2[line_index][4:])  #跳过 '编号','姓名','年龄','住院号',
+                    line_index += 1
         
         ws.append(outrow)
+        lastrow=list(outrow)  #保存之前处理过的行
         
     print('解析完毕！')
 
