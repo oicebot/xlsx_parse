1eb5748c3b4b89dd3c1a065855547137511ffade
KaimingTao <mads396@gmail.com>
2017-05-22 15:45:38 +0800

    update sanitize.py

--- sanitize.py
+++ sanitize.py
@@ -8,6 +8,7 @@
 
 import argparse
 from openpyxl import load_workbook
+import os
 
 # 导入调试用工具，彩色打印，区分 print 打印 debug 打印 exception 打印
 
@@ -40,16 +41,72 @@ def parse_command():
     return args
 
 
-def getTable(excel_file_path):
-    # 使用绝对地址
-    workbook = load_workbook(excel_file_path)
+def get_sheets(excel_file_path):
 
-    tables = []
+    base_path = os.path.dirname(
+        os.path.abspath(__file__)
+    )
+
+    file_path = os.path.join(base_path, excel_file_path)
+
+    workbook = load_workbook(file_path)
+
+    sheets = []
 
     for sheet in workbook:
-        tables.append(sheet)
+        sheets.append(sheet)
 
+    return sheets
+
+
+def list_all_sheets(sheets):
+
+    sheet_ids = []
+
+    for index, item in enumerate(sheets):
+        print(index, ':', item.title)
+        sheet_ids.append(index)
+
+    return sheet_ids
+
+
+def load_sheet_from_input(sheets):
+
+    selected_sheet = None
+
+    while True:
+        sheet_ids = list_all_sheets(sheets)
+
+        user_selection = input('请输入你想要解析的工作表的编号：')
+
+        if not user_selection.isdigit():
+            print('请输入对应的数字')
+            continue
+
+        sheet_id = int(user_selection)
+
+        if sheet_id not in sheet_ids:
+            print('您输入的 ID 不存在，')
+            continue
+
+        selected_sheet = sheets[sheet_id]
+        print('')
+        break
+
+
+    print('已打开表："', selected_sheet.title, '",解析中…')
+    return selected_sheet
+
+
+def save_workbook(cleaned_data):
+
+    pass
 
 if __name__ == '__main__':
     args = parse_command()
     print(args)
+    sheets = get_sheets(args.file)
+
+
+
+    load_sheet_from_input(sheets)
